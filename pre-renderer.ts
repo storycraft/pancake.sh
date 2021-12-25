3de43113d5eeaca484ffaa80023c40acaea8e4e1
storycraft <storycraft@pancake.sh>
2021-12-25 19:55:34 +0900

    Pre render styles

--- pre-renderer.ts
+++ pre-renderer.ts
@@ -9,16 +9,21 @@ import { createElement } from 'react';
 import { renderToString } from 'react-dom/server';
 import { PancakeApp } from './frontend/app';
 import * as cheerio from 'cheerio';
+import { ServerStyleSheet } from 'styled-components';
 
 const CACHED_DIR = '.ssr';
 
 const template = readFileSync('frontend/index.html', 'utf-8');
-const app = renderToString(createElement(PancakeApp, {}, null));
+const app = createElement(PancakeApp, {}, null);
+
+const sheet = new ServerStyleSheet();
+renderToString(sheet.collectStyles(app));
 
 mkdirSync(CACHED_DIR, { recursive: true });
 
 const parsed = cheerio.load(template);
-parsed('#root').html(app);
+parsed('head').append(sheet.getStyleTags());
+parsed('#root').html(renderToString(app));
 
 writeFileSync(`${CACHED_DIR}/index.html`, parsed.html(), 'utf-8');
 
